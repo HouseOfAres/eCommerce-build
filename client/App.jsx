1e8705e3c8bb15a7e3d9c9f301046d02ec69f3cf
Sophia Nguyen <nguyen.x.sophia@gmail.com>
2021-07-23 15:36:25 -0700

    Adjusted App.jsx React Components

--- client/App.jsx
+++ client/App.jsx
@@ -6,24 +6,15 @@ import Ratings from './components/ratings-review/Ratings.jsx';
 import Related from './components/related/Related.jsx';
 import '../public/styles.css';
 
-// CHANGE NAME HERE
+
 const App = () => {
-//
-// // Returns a stateful value, and a function to update it.
-//   const [state, setState] = useState(initialState);
-
-//
-// // Accepts a function that contains imperative, possibly effectful code.
-
-//
-// // Accepts a context object (the value returned from React.
 
   return (
     <div>
       <Overview />
-      <Ratings />
-      <QuestionsAndAnswers />
       <Related />
+      <QuestionsAndAnswers />
+      <Ratings />
     </div>
   )
 
