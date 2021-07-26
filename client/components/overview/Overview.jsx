4db2054345d0b0e082d0b2d74f4569a8e8d1e0df
chanychi <chan.ychi@gmail.com>
2021-07-26 14:03:15 -0500

    images

--- client/components/overview/Overview.jsx
+++ client/components/overview/Overview.jsx
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import styles from '../../../mock-data/styles-data.js';
 
 // import React { useState } from 'react;'
 
 // CHANGE NAME HERE
 const Overview = () => {
-
+// console.log(styles.productStyles.results[0].photos)
 
 
   return (
     <div className="test">
       <h1>HOUSE OF ARES - CHI'S AREA</h1>
+      <img src={styles.productStyles.results[0].photos[0].thumbnail_url}></img>
+      <img src={styles.productStyles.results[0].photos[1].thumbnail_url}></img>
+      <img src={styles.productStyles.results[0].photos[2].thumbnail_url}></img>
+      <img src={styles.productStyles.results[0].photos[3].thumbnail_url}></img>
+      <img src={styles.productStyles.results[0].photos[4].thumbnail_url}></img>
+      <img src={styles.productStyles.results[0].photos[5].thumbnail_url}></img>
     </div>
   )
 
