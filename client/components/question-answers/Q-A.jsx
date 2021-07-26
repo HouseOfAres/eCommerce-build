4bc6c3e119c1d31530f70cee0cfadbf830f72832
Tyler Petersen <tylerpetersen02@gmail.com>
2021-07-26 14:02:15 -0700

    modifying layout for QA component

--- client/components/question-answers/Q-A.jsx
+++ client/components/question-answers/Q-A.jsx
@@ -5,24 +5,32 @@ import React, { useState, useEffect } from 'react';
 // CHANGE NAME HERE
 const App = () => {
 
-// Returns a stateful value, and a function to update it.
-//   const [state, setState] = useState(initialState);
-//   setState(newState);
+  // Returns a stateful value, and a function to update it.
+  //   const [state, setState] = useState(initialState);
+  //   setState(newState);
 
 
-// // Accepts a function that contains imperative, possibly effectful code.
-//   useEffect(fn);
+  // // Accepts a function that contains imperative, possibly effectful code.
+  //   useEffect(fn);
 
 
-// // Accepts a context object (the value returned from React.
-//   const value = useContext(MyContext);
+  // // Accepts a context object (the value returned from React.
+  //   const value = useContext(MyContext);
 
-// this is a comment... bruh.
+  // this is a comment... bruh.
 
   return (
     <div className="component">
-      <h2>YO WE ARE ARES BRUH!!!!!!!!!!!!!</h2>
-      <h3>Hello there</h3>
+      <div className="q_a_component">
+        <span>QUESTIONS & ANSWERS</span>
+        <div className="searchBar"></div>
+        <div className="q_a_subcomponent">
+          <div className="questions"></div>
+          <div className="answers"></div>
+        </div>
+        <div className="q_a_buttons"></div>
+      </div>
+
     </div>
   )
 
