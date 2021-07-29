fe05c54ab99af1271be69bbacadb3287fc30b4ac
chanychi <chan.ychi@gmail.com>
2021-07-29 14:49:46 -0500

    style thumb rendering

--- client/components/overview/Overview.jsx
+++ client/components/overview/Overview.jsx
@@ -4,42 +4,69 @@ import productInfo from '../../../mock-data/product-information.js';
 import Thumbnail from './Thumbnail.jsx';
 import Styles from './Styles.jsx';
 // import React { useState } from 'react;'
+import axios from 'axios';
+import access from '../../../config.js';
 import { ProductContext } from '../../ProductContext.jsx';
 
 // CHANGE NAME HERE
 const Overview = () => {
-  console.log(useContext(ProductContext))
+  let id = (useContext(ProductContext))
+  // console.log(id.id)
   // console.log(image.productStyles.results[0].photos[0].thumbnail_url)
   const [main, setMain] = useState(image.productStyles.results[0].photos[0].url);
-  const [nail, setNail] = useState(image.productStyles.results[0]);
+  const [nail, setNail] = useState(image.productStyles.results[0].photos);
+  const [sty, setSty] = useState([])
 
-  const imageHandler = (event) => {
-    setMain(event)
+  const imageHandler = (evt) => {
+    setMain(evt.url)
     // console.log(event)
+    // setNail(evt.thumbnail_url)
   }
+
+  const styleHandler = (evt) => {
+    setMain(evt.photos[0].url)
+    setNail(evt.photos)
+  }
+
+
+  useEffect(()=> {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${id.id}/styles`, {headers: {'Authorization': `${access.TOKEN}`}
+          })
+          .then((response) => {
+            setMain(response.data.results[0].photos[0].url);
+            setNail(response.data.results[0].photos)
+            setSty(response.data.results)
+            // console.log(response.data.results)
+
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+  },[id])
+
 
   return (
     <div className="component">
       <div className="productOverView">
         <img className="Main-ImageOV" src={main}></img>
         <div className="thumbnailList">
-        {nail.photos.map((e) => {
+        {nail.map((e) => {
           return <Thumbnail thumb={e} imageHandle={imageHandler} />
         })}
         </div>
         <div className="category">
           <p>STARS</p>
           <p className="cate">CATEGORY</p>
-          <div className="category-item"><h3>{productInfo.productInformation.category}</h3></div>
+          <div className="category-item"><h3>{id.category}</h3></div>
           </div>
           <div className="product-priceOV">
-          ${productInfo.productInformation.default_price}
+          ${id.default_price}
           </div>
           <div className="styleName"><h3>STYLE > FOREST GREEN & BLACK</h3></div>
           <div className="stylescontainerOV">
 
-            {image.productStyles.results.map((e) => {
-              return <Styles style={e} imageHandle={imageHandler}/>
+            {sty.map((e) => {
+              return <Styles style={e} styleHandle={styleHandler}/>
             })}
 
           </div>
