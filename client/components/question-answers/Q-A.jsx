319058e761593650f0d046adbf5549e56514bfd2
Tyler Petersen <tylerpetersen02@gmail.com>
2021-07-29 16:58:09 -0700

    completed search bar functionality

--- client/components/question-answers/Q-A.jsx
+++ client/components/question-answers/Q-A.jsx
@@ -3,6 +3,8 @@ import questions from '../../../mock-data/questions-data.js';
 import QuestionsList from './QuestionsList.jsx';
 import SearchBar from './SearchBar.jsx';
 import QaButtons from './Q-A-Buttons.jsx';
+import access from '../../../config.js';
+import axios from 'axios';
 
 // import React { useState } from 'react;'
 
@@ -12,11 +14,22 @@ const App = () => {
   const questionData = questions.questions.results;
   const [questionList, setQuestionList] = useState(questionData.slice(0, 4));
 
+  useEffect(()=> {
+    axios.get('...', {headers: {'Authorization': `${access.TOKEN}`}
+          })
+          .then((response) => {
+            setProductReviews(response.data.results);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+  },[]);
+
   return (
     <div className="component">
       <div className="q_a_component">
         <div className="component_title">QUESTIONS & ANSWERS</div>
-        <SearchBar />
+        <SearchBar questionData={questionData} setQuestionList={setQuestionList}/>
         <QuestionsList questionList={questionList} />
         <QaButtons questionData={questionData} questionList={questionList} setQuestionList={setQuestionList}/>
       </div>
--- client/components/question-answers/SearchBar.jsx
+++ client/components/question-answers/SearchBar.jsx
@@ -1,13 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import questions from '../../../mock-data/questions-data.js';
 
 const SearchBar = (props) => {
 
+  const questionData = questions.questions.results;
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const [queryInit, setQueryInit] = useState(questionData);
+  const [noMatches, setNoMatches] = useState(false);
+
+
+  useEffect(() => {
+    setNoMatches(false);
+    if (searchQuery === '') {
+      props.setQuestionList(questionData.slice(0, 4));
+    } else {
+      const currentQueryResults = [];
+      questionData.forEach(question => {
+        const lowercase = question.question_body.toLowerCase();
+        if (lowercase.includes(searchQuery.toLowerCase())) {
+          currentQueryResults.push(question);
+        }
+        if (currentQueryResults.length) {
+          setQueryInit(currentQueryResults);
+
+          props.setQuestionList(queryInit);
+          setNoMatches(false);
+        } else {
+          props.setQuestionList([])
+
+          setNoMatches(true);
+
+        }
+
+      })
+    }
+  }, [searchQuery]);
+
+  const searchChangeHandler = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  // onChange should search through all the available questions and display the results
+  // should check to see if the questions include the text written in the search bar
+  // should ignore upper/lower case
+  // pass all questions as props to searchbar but set the display to null
+  // onChange, update the questionList in Q-A
+  // or if search is empty, show questionList
+  // if text is entered in search, render new list of questions with matching text
   return (
     <div className="searchBar_container">
       <form>
-        <input className="searchbar" type="text" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS...">
+        <input
+          className="searchbar"
+          type="text"
+          placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."
+          onChange={searchChangeHandler}
+        >
         </input>
       </form>
+      {noMatches === true && <h3 className="noMatch">Oops! Nothing to see here...</h3>}
     </div>
   )
 };
